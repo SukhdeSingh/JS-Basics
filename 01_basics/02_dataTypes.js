2facbb07833f97f3ef61cd3ce95997e9922ed15b
Sukhdev Singh <yadav555sukhdev@gmail.com>
2026-02-14 10:32:57 +0000

    dataTypes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all JS code as newer version of js
+
+// alert( 3 + 3) // we are now using nodejs, not browser
+
+//dataTypes
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number =>  2 to power
+// bigint
+// string= ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol
+
+// object
+
+
+console.log(typeof "aman"); // string
+console.log(typeof undefined) // undefined
+console.log(typeof null); // object
