b65d00738b15986ae70ba05db250bc3640d3de23
Sukhdev Singh <yadav555sukhdev@gmail.com>
2026-02-14 08:15:47 +0000

    container dev

---
+++ 01_basics/test.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+accountEmail = "hc@hc.com"
+accountPassword = "78987"
+accountCity = "Bengluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
