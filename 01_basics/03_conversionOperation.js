b6bddea341e93b93aad85b72bfd916ea096ee486
Sukhdev Singh <yadav555sukhdev@gmail.com>
2026-02-15 06:36:33 +0000

    dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+// number to string conversion
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ******************* Operations ******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
