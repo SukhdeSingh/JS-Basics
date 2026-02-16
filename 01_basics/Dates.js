0634ca67d48287f199d5d9d6263829d0993c11b0
Sukhdev Singh <yadav555sukhdev@gmail.com>
2026-02-16 07:59:04 +0000

    dates in js

---
+++ 01_basics/Dates.js
@@ -0,0 +1,10 @@
+//Dates
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date(2023, 0, 21)
+console.log(myCreatedDate);
+
