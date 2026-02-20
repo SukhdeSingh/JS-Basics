36c788a1ca806a4de588b4a018102a3549f1b3b4
Sukhdev Singh <yadav555sukhdev@gmail.com>
2026-02-20 07:30:30 +0000

    conditional statement in js

---
+++ 04_control_flow/ONE.js
@@ -0,0 +1,39 @@
+// if
+//const isUserloggedIn = true;
+
+// if ( 2 == "2"){
+// console.log("executed");
+// }
+
+// but === is compare the value and dataTypes also
+//if( 2 === "2"){
+   // console.log("executed");//here the loop will not runs because 
+    //2 = 2 but there data types is not equal, one is intger and another is string
+//}
+
+// <, >, <=, >=, ==, !=, ===
+
+//switch case syntaax
+
+// switch (key) {
+//     case value:
+
+//     break;
+
+//     default: 
+//     break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+// console.log("January");
+//     break;
+// case 2:
+//     console.log("february");
+//     default:
+//         console.log("april");
+//         break;
+// }
+
