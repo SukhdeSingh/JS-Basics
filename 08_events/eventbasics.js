57700e04e1181dcd8a96fe5d0c24067740e19199
Sukhdev Singh <yadav555sukhdev@gmail.com>
2026-02-25 11:03:41 +0000

    js concept

---
+++ 08_events/eventbasics.js
@@ -0,0 +1,13 @@
+// Javascript is synchronous means one by one task is executed
+// js is also single threaded language means all of will executed on single thread
+
+//Execution content
+//execute one line of code at a time and each operation waits for the last one 
+// to complete before execution
+
+//Blocking code
+//Block the flow of Program -> Read file Sync
+
+//Non Blocking code
+//Does not block execution -> Read file Async
+
